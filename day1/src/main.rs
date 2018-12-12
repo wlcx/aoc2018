62d7137f5cc78c5add73c4cbb606d7bb8ce20aa0
Sam Willcocks <sam@wlcx.cc>
2018-12-12 21:53:02 +0000

    Add working day1

---
+++ day1/src/main.rs
@@ -0,0 +1,15 @@
+use std::io::*;
+use std::fs::File;
+use std::str::FromStr;
+
+fn main() {
+    let f = File::open("input.txt").unwrap();
+    let mut accumulator: i32 = 0;
+    for line in BufReader::new(f).lines() {
+        let l = line.unwrap();
+        if l != "" {
+        accumulator += i32::from_str(&l).unwrap();
+        }
+    }
+    println!("Result: {}", accumulator);
+}
